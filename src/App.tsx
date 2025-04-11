2eaa7e59c0683d2f2aaa623a4e8f681b8d625fee
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-04-10 22:14:12 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CanvasEditor from './components/CanvasEditor';
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <CanvasEditor />
+    </div>
+  );
+}
+
+export default App;
