362b95bff2d7f88669dd8683a1ebad5ebac06b5c
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-04-29 21:55:26 -0400

    w

--- src/socket.ts
+++ src/socket.ts
@@ -1,9 +1,12 @@
 import { io, Socket } from "socket.io-client";
 
-const protocol = window.location.protocol === "https:" ? "wss" : "ws";
-const host = window.location.hostname;
-const port = window.location.port === "5173" ? "3000" : window.location.port;
+const socketUrl =
+  import.meta.env.PROD
+    ? "wss://backsw1.onrender.com" // ✅ Producción (Vercel)
+    : "ws://localhost:3000";        // ✅ Desarrollo local
+    console.log(import.meta.env.PROD); // true en Vercel, false en localhost
 
-export const socket: Socket = io(`${protocol}://${host}:${port}`, {
+export const socket: Socket = io(socketUrl, {
   transports: ["websocket"],
+  
 });
